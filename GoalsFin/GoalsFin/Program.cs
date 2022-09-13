af19d9e140f4e1403132289758f948f89cdf7c9c
Edge Nicholson <edge_nicholson@progressive.com>
2022-09-13 10:58:09 -0400

    new Goals repo

---
+++ GoalsFin/GoalsFin/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your Goal for today:");
+Console.WriteLine("---------------------------------");
+string? goal = Console.ReadLine();
+Console.WriteLine($"\n[For {DateTime.Now.ToString("dddd, MMMM dd, yyyy")} Your Goal Is:]");
+Console.WriteLine($"\n\"{goal}\"");
+Console.Write("\n[Save Changes (Y/n)] ");
+char? answer = Console.ReadLine()[0];
