90a62f27e2219118f5a585813bf30be1ec516fdd
Edge Nicholson <edge_nicholson@progressive.com>
2022-09-13 16:31:27 -0400

    Finished 6 tests up to task 3

--- stringcalculator-tuesday/StringCalculator.cs
+++ stringcalculator-tuesday/StringCalculator.cs
@@ -1,11 +1,31 @@
 ﻿
+using System.Numerics;
+
 namespace StringCalculator;
 
 public class StringCalculator
 {
-
+    public char[] delimeters = { ',', '\n'};
     public int Add(string numbers)
     {
-        return -42;
+        if (numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        if (!(numbers.Contains(",")| numbers.Contains("\n")))
+        {
+            return int.Parse(numbers);
+        }
+        else
+        {
+            string[] numbersArray = numbers.Split(delimeters);
+            int totalSum = 0;
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                totalSum += int.Parse(numbersArray[i].Trim());
+            }
+            return totalSum;
+        }
     }
 }
--- stringcalculator-tuesday/StringCalculatorTests.cs
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -12,4 +12,45 @@ public class StringCalculatorTests
 
         Assert.Equal(0, result);
     }
+
+    [Fact]
+    public void SingleStringReturnsSameNumber()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1");
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void TwoNumbersReturnSum()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1,2");
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void ManyNumbersReturnsSum()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1,2,3,4");
+        Assert.Equal(10, result);
+    }
+
+    [Fact]
+    public void SumUsingNewLineDelimeter()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1\n2\n3\n4");
+        Assert.Equal(10, result);
+    }
+
+    [Fact]
+    public void SumUsingBothDelimeter()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1,2\n3,4");
+        Assert.Equal(10, result);
+    }
+
 }
