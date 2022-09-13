c6484477c221be2157f94e92f4fe42c32c4492bb
Edge Nicholson <edge_nicholson@progressive.com>
2022-09-13 12:25:42 -0400

    updated

--- CSharpSyntaxStuff/CreatingInstancesOfTypes.cs
+++ CSharpSyntaxStuff/CreatingInstancesOfTypes.cs
@@ -1,11 +1,46 @@
 ﻿
+using System.Collections;
+using System.Runtime.InteropServices;
+
 namespace CSharpSyntaxStuff;
 
 public class CreatingInstancesOfTypes
 {
     [Fact]
-    public void DoIt()
+    public void DeterminingTypes()
     {
-        Assert.True(false);
+        var favoriteNumbers = new List<int>();
+        favoriteNumbers.Add(10);
+        favoriteNumbers.Add(20);
+        
+        var sumOfFirstTwo = favoriteNumbers[0] + favoriteNumbers[1];
+        Assert.Equal(30, sumOfFirstTwo);
     }
+
+    [Fact] 
+    public void OldSchoolCollections()
+    {
+        var favoriteNumbers = new ArrayList(); favoriteNumbers.Add(9); favoriteNumbers.Add(42); favoriteNumbers.Add(20);
+
+        Assert.Equal(9, favoriteNumbers[0]);
+
+        favoriteNumbers.Add("Pizza");
+
+        favoriteNumbers[0] = "Three";        
+        var sumOfFirstTwo = ((int)favoriteNumbers[0]) + ((int)favoriteNumbers[1]);
+
+        Assert.Equal(51, sumOfFirstTwo);
+    }
+
+    [Fact]
+    public void NewSchoolCollections()
+    {        // Parametric Polymorphism        
+        var favoriteNumbers = new List<int>();
+
+        favoriteNumbers.Add(9); favoriteNumbers.Add(42); favoriteNumbers.Add(20);
+
+        var sumOfFirstTwo = favoriteNumbers[0] + favoriteNumbers[1]; Assert.Equal(51, sumOfFirstTwo);
+    }
+
+
 }
