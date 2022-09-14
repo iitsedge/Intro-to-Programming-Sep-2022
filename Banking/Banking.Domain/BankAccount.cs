b5b1cbab95ad92dc0e01a01b8891778eb283c444
Edge Nicholson <edge_nicholson@progressive.com>
2022-09-14 14:12:18 -0400

    new Banking sln

---
+++ Banking/Banking.Domain/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿namespace Banking.Domain;
+
+public class BankAccount
+{
+
+    private decimal _balance = 5000M; //JFHCI
+    public void Deposit(decimal amountToDeposit)
+    {
+        _balance += amountToDeposit;
+    }
+
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        if (AccountHasAvailableFunds(amountToWithdraw))
+        {
+            _balance -= amountToWithdraw;
+        }
+        else
+        {
+            throw new OverdraftException();
+        }
+    }
+
+    private bool AccountHasAvailableFunds(decimal amountToWithdraw)
+    {
+        return amountToWithdraw <= _balance;
+    }
+}
