778b036975d7b87f21afc8326debb08f717297da
Edge Nicholson <edge_nicholson@progressive.com>
2022-09-16 12:31:17 -0400

    Before Lunch Friday

---
+++ BankingApiSolution/Banking.IntegrationTests/AccountResourceTests.cs
@@ -0,0 +1,22 @@
+﻿using Alba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.IntegrationTests;
+public class AccountResourceTests
+{
+    [Fact]
+    public async Task AccountResourcesReturnsAnOk()
+    {
+        await using var host = await AlbaHost.For<global::Program>(config => { });
+
+        await host.Scenario(api =>
+        {
+            api.Get.Url("/accounts");
+            api.StatusCodeShouldBeOk(); // response status is a 200
+        });
+    }
+}
