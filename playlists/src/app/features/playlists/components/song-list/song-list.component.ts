11240fd5277e69dc2770bd9a0bcc75466a9c44a8
Edge Nicholson <edge_nicholson@progressive.com>
2022-09-20 15:17:07 -0400

    before end Tuesday

---
+++ playlists/src/app/features/playlists/components/song-list/song-list.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { SongListModel } from '../../models';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-song-list',
+  templateUrl: './song-list.component.html',
+  styleUrls: ['./song-list.component.css'],
+})
+export class SongListComponent {
+  model: Observable<SongListModel>;
+
+  myPay = 42.23;
+  constructor(private client: HttpClient) {
+    this.model = this.client.get<SongListModel>(environment.apiUrl + 'songs');
+  }
+}
